bb6d165be61e6baaf02c3f99e01bf0430c908cfe
andre.devito@aditusbr.com <andre_devito@hotmail.com>
2019-12-05 16:34:44 -0300

    v1.0

---
+++ WebContent/WEB-INF/views/conta/lista.jsp
@@ -0,0 +1,48 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Lista de Contas</title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<th>Código</th>
+			<th>Descrição</th>
+			<th>Valor</th>
+			<th>Tipo</th>
+			<th>Paga?</th>
+			<th>Data de Pagamento</th>
+			<th>Ações</th>
+		</tr>
+
+		<c:forEach items="${listContas}" var="conta">
+		<tr>
+			<td>${conta.id}</td>
+			<td>${conta.descricao}</td>
+			<td>${conta.valor}</td>
+			<td>${conta.tipo}</td>
+			<td>
+				<c:if test="${conta.paga eq false}">
+				Não Paga
+				</c:if>
+				<c:if test="${conta.paga eq true}">
+				Paga
+				</c:if>
+			</td>
+			<td><fmt:formatDate value="${conta.dataPagamento.time}" pattern="dd/MM/yyyy" /></td>
+			<td>
+				<a href="removeConta?id=${conta.id}">Deletar</a>
+				<a href="mostraConta?id=${conta.id}">Alterar</a>
+			</td>
+		</tr>	
+		</c:forEach>
+	</table>
+
+</body>
+</html>
---
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -0,0 +1,86 @@
+package br.com.caelum.contas.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.contas.dao.ContaDAO;
+import br.com.caelum.contas.modelo.Conta;
+
+@Controller
+public class ContaController {
+
+	@RequestMapping("/conta/form")
+	public String formulario() {
+		return "conta/formulario";
+	}
+	
+	@RequestMapping("/conta/adicionaConta")
+	public String adiciona(Conta conta) {
+		ContaDAO contaDAO = new ContaDAO();
+		contaDAO.adiciona(conta);
+		
+		return "conta/conta-adicionada";
+	}
+	
+//	@RequestMapping("/conta/listaContas")
+//	public ModelAndView lista() {
+//		ContaDAO contaDAO = new ContaDAO();
+//		List<Conta> listContas = contaDAO.lista();
+//		
+//		ModelAndView mv = new ModelAndView("conta/lista");
+//		mv.addObject("listContas", listContas);
+//		
+//		return mv;
+//	}
+	
+	@RequestMapping("/conta/listaContas")
+	public String lista(Model m) {
+		ContaDAO contaDAO = new ContaDAO();
+		List<Conta> listContas = contaDAO.lista();
+		
+		m.addAttribute("listContas", listContas);
+		
+		return "/conta/lista";
+	}
+	
+//	@RequestMapping("/conta/removeConta")
+//	public ModelAndView remove(Conta conta) {
+//		ContaDAO contaDAO = new ContaDAO();
+//		contaDAO.remove(conta);
+//		
+//		List<Conta> listContas = contaDAO.lista();
+//		ModelAndView mv = new ModelAndView("conta/lista");
+//		mv.addObject("listContas", listContas);
+//		
+//		return mv;
+//	}
+	
+	@RequestMapping("/conta/removeConta")
+	public String remove(Conta conta) {
+		ContaDAO contaDAO = new ContaDAO();
+		contaDAO.remove(conta);
+		
+		return "redirect:/conta/listaContas";
+	}
+	
+	@RequestMapping("/conta/mostraConta")
+	public String mostra(Long id, Model model) {
+		ContaDAO contaDAO = new ContaDAO();
+		model.addAttribute("conta", contaDAO.buscaPorId(id));
+		
+		return "/conta/conta";
+	}
+	
+	@RequestMapping("/conta/alteraConta")
+	public String altera(Conta conta) {
+		ContaDAO contaDAO = new ContaDAO();
+		contaDAO.altera(conta);
+		
+		return "redirect:/conta/listaContas";
+	}	
+	
+}
